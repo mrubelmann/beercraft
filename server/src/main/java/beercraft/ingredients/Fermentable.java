68e289295478af1f5931aad20da6bd354c997b95
Mark Rubelmann <mark.rubelmann@eagleview.com>
2018-09-11 09:13:39 -0400

    Added DynamoDBMapper annotations to Fermentable POJO.

--- server/src/main/java/beercraft/ingredients/Fermentable.java
+++ server/src/main/java/beercraft/ingredients/Fermentable.java
@@ -1,5 +1,10 @@
 package beercraft.ingredients;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable(tableName = "Fermentables")
 public class Fermentable implements Ingredient {
     private String id;
     private String name;
@@ -8,50 +13,48 @@ public class Fermentable implements Ingredient {
     private double defaultTimeInMinutes;
     private double color;
 
+    @DynamoDBHashKey(attributeName = "Id")
     public String getId() {
         return id;
     }
-
     public void setId(String id) {
         this.id = id;
     }
 
+    @DynamoDBAttribute(attributeName = "Name")
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
 
+    @DynamoDBAttribute(attributeName = "Notes")
     public String getNotes() {
         return notes;
     }
-
     public void setNotes(String notes) {
         this.notes = notes;
     }
 
+    @DynamoDBAttribute(attributeName = "DefaultAmount")
     public double getDefaultAmount() {
         return defaultAmount;
     }
-
     public void setDefaultAmount(double defaultAmount) {
         this.defaultAmount = defaultAmount;
     }
 
+    @DynamoDBAttribute(attributeName = "DefaultTimeInMinutes")
     public double getDefaultTimeInMinutes() {
         return defaultTimeInMinutes;
     }
+    public void setDefaultTimeInMinutes(double defaultTimeInMinutes) { this.defaultTimeInMinutes = defaultTimeInMinutes; }
 
-    public void setDefaultTimeInMinutes(double defaultTimeInMinutes) {
-        this.defaultTimeInMinutes = defaultTimeInMinutes;
-    }
-
+    @DynamoDBAttribute(attributeName = "Color")
     public double getColor() {
         return color;
     }
-
     public void setColor(double color) {
         this.color = color;
     }
