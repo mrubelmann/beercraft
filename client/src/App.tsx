51647a4b07d038dabf5aab0245dff1bbe1014b4c
Mark Rubelmann <mrubelmann@gmail.com>
2018-10-19 00:00:20 -0400

    Got rid of default and added a title bar.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,30 @@
 import * as React from 'react';
-import './App.css';
 
-import logo from './logo.svg';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              Beercraft
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
