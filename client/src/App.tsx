fb7bacfca9c0ce08eb8d4117919d540d39b4560c
Mark Rubelmann <mrubelmann@gmail.com>
2018-10-21 10:22:30 -0400

    Did some styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import './styles/App.css';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
--- client/src/RecipeList.tsx
+++ client/src/RecipeList.tsx
@@ -1,31 +1,13 @@
 import * as React from 'react';
+import './styles/RecipeList.css';
 
-import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
-const styles = {
-    card: {
-      minWidth: 275,
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-};
-
 function RecipeList(props: any) {
-    const { classes } = props;
     const recipes = [
         { name: 'Recipe 1', style: 'IPA' },
         { name: 'Recipe 2', style: 'Scotch Ale' },
@@ -40,13 +22,13 @@ function RecipeList(props: any) {
     ];
 
     return (
-        recipes.map((recipe) => (
-            <Card className={classes.card}>
+        recipes.map((recipe, i) => (
+            <Card key={i} className="recipe-card">
                 <CardContent>
                     <Typography variant="h5" component="h2">
                         {recipe.name}
                     </Typography>
-                    <Typography className={classes.pos} color="textSecondary">
+                    <Typography className="pos" color="textSecondary">
                         style
                     </Typography>
                     <Typography component="p">
@@ -61,4 +43,4 @@ function RecipeList(props: any) {
     );
 }
 
-export default withStyles(styles)(RecipeList as any);
+export default RecipeList as any;
