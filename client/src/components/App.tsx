d4b7da7397d0f9f556982a61c1f452b5b4988d8d
Mark Rubelmann <mrubelmann@gmail.com>
2018-10-24 23:08:28 -0400

    Moved components into subdirectory.

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import './styles/App.css';
+import '../styles/App.css';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
--- client/src/components/RecipeList.tsx
+++ client/src/components/RecipeList.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import './styles/RecipeList.css';
+import '../styles/RecipeList.css';
 
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
