ed218607f1040b626f119878faa3a1ab10d18cff
Mark Rubelmann <mrubelmann@gmail.com>
2018-12-30 09:44:28 -0500

    Added another unit test.

--- server/src/test/java/beercraft/users/GetUserDatabaseRequestWorkerTest.java
+++ server/src/test/java/beercraft/users/GetUserDatabaseRequestWorkerTest.java
@@ -24,4 +24,27 @@ class GetUserDatabaseRequestWorkerTest {
         Fermentable actualFermentable = FermentableFactory.createFromAttributes(queryResult.get(0));
         assertThat(actualFermentable).isEqualToComparingFieldByField(fermentable);
     }
+
+    @Test
+    void execute_twoFermentables_allRecordsAreReturned() {
+        Fermentable[] fermentables = new Fermentable[] {
+                new FermentableBuilder().build(),
+                new FermentableBuilder().build()
+        };
+        GetGlobalIngredientsQuery getGlobalIngredientsQuery = new MockGetGlobalIngredientQueryBuilder()
+                .withFermentable(fermentables[0])
+                .withFermentable(fermentables[1])
+                .build();
+
+        GetUserDatabaseRequestWorker worker = new GetUserDatabaseRequestWorker(getGlobalIngredientsQuery);
+        QueryResult queryResult = worker.execute();
+        assertThat(queryResult.size()).isEqualTo(2);
+
+        Fermentable[] actualFermentables = new Fermentable[]{
+                FermentableFactory.createFromAttributes(queryResult.get(0)),
+                FermentableFactory.createFromAttributes(queryResult.get(1))
+        };
+        assertThat(actualFermentables[0]).isEqualToComparingFieldByField(fermentables[0]);
+        assertThat(actualFermentables[1]).isEqualToComparingFieldByField(fermentables[1]);
+    }
 }
